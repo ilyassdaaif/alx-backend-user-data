e7781d28a1e35eb5d0765a3d6db068f7be749739
ilyassdf <ilyassdf@gmail.com>
2024-08-29 20:27:41 +0000

    a

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,12 +8,8 @@ from typing import List
 import logging
 
 
-def filter_datum(
-        fields: List[str],
-        redaction: str,
-        message: str,
-        separator: str
-) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
     Obfuscates specified fields in the log message.
     """
