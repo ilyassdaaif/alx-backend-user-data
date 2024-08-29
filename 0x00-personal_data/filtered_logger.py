3624bdee051a8dbaba257a06ae64f4a91f8c9c5a
ilyassdf <ilyassdf@gmail.com>
2024-08-29 20:13:57 +0000

    a

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -26,3 +27,24 @@ def filter_datum(
     """
     pattern = f'({"|".join(map(re.escape, fields))})[^{separator}]*'
     return re.sub(pattern, f'\\1={redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(
+                self.fields,
+                self.REDACTION,
+                record.getMessage(),
+                self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
