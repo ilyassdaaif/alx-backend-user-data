717e2a556f9027694706b53d68a699d6b910d808
ilyassdf <ilyassdf@gmail.com>
2024-08-30 01:19:19 +0000

    a

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,12 +3,21 @@
 Module for obfuscating sensitive information in log messages.
 """
 
-import re
 import logging
+import re
 from typing import List
 
+# Define PII_FIELDS as a tiple of PII field names
+PII_FIELDS = ("name", "email", "ssn", "phone", "password")
 
-def filter_datum(fields, redaction, message, separator):
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str
+) -> str:
+    """Returns the log message obfuscated."""
     return re.sub(
         '|'.join([f'{field}=[^;]+' for field in fields]),
         lambda match: match.group().split('=')[0] + f'={redaction}',
@@ -17,6 +26,8 @@ def filter_datum(fields, redaction, message, separator):
 
 
 class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class."""
+
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
@@ -30,3 +41,23 @@ class RedactingFormatter(logging.Formatter):
         return filter_datum(
             self.fields, self.REDACTION, message, self.SEPARATOR
         )
+
+    def get_logger():
+        """Creates and returns a logger."""
+        logger = logging.getLogger("user_data")
+        logger.setLevel(logging.INFO)
+        logger.propagate = False
+
+        # Create a StreamHandler
+        stream_handler = logging.StreamHandler()
+
+        # Create a RedactingFormatter with PII_FIELDS
+        formatter = RedactingFormatter(PII_FIELDS)
+
+        # Set the formatter for the handler
+        stream_handler.setFormatter(formatter)
+
+        # Add the handler to the logger
+        logger.addHandler(stream_handler)
+
+        return logger
