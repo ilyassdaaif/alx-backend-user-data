02da23ff0cd2256b3406cee28bd06ce882530700
ilyassdf <ilyassdf@gmail.com>
2024-09-05 14:31:53 +0000

    a

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """BasicAuth class for the API"""
 from api.v1.auth.auth import Auth
-
+import base64
 
 class BasicAuth(Auth):
     """BasicAuth class to manage API authentication"""
@@ -21,3 +21,20 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+    ) -> str:
+        """
+        Decodes the Base64 string base64_authorization_header.
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
