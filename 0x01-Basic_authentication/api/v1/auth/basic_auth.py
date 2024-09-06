6ff09632fa43dc45051cc027f28f2920e9e001e5
ilyassdf <ilyassdf@gmail.com>
2024-09-06 11:37:32 +0000

    a

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@
 from api.v1.auth.auth import Auth
 import base64
 from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -56,3 +57,28 @@ class BasicAuth(Auth):
 
         credentials = decoded_base64_authorization_header.split(':', 1)
         return credentials[0], credentials[1]
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
+        """
+        Returns the User instance based on email and password.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        if not users:
+            return None
+
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
