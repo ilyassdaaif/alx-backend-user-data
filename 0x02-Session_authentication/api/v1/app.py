80612d58e0b7707d42b9632c7ed6bbac7fcf2f36
ilyassdf <ilyassdf@gmail.com>
2024-09-07 11:54:10 +0000

    a

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -26,6 +26,27 @@ elif AUTH_TYPE == "session_auth":
     auth = SessionAuth()
 
 
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.before_request
 def before_request():
     """Filter requests before processing"""
