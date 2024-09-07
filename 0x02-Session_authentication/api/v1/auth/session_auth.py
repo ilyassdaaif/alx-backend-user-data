0509850e8562b5d641a7c8687b4dd7c41161e352
ilyassdf <ilyassdf@gmail.com>
2024-09-07 19:51:40 +0000

    p

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Session authentication module"""
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
-    """Session Authentication class"""
+    """Session Authentication"""
 
     user_id_by_session_id = {}
 
@@ -23,3 +24,21 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Returns a User instance based on a cookie value """
+        # Retrieve the session ID from the request's cookie
+        session_id = self.session_cookie(request)
+
+        if session_id is None:
+            return None
+
+        # Get the user ID linked to this session ID
+        user_id = self.user_id_for_session_id(session_id)
+
+        if user_id is None:
+            return None
+
+        # Retrieve and return the User instance
+        user = User.get(user_id)
+        return user
