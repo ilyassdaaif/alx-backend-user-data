27432b587ee07f6fb95b283e6b89e373d3f96577
ilyassdf <ilyassdf@gmail.com>
2024-09-07 17:47:08 +0000

    a

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -5,7 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS
 import os
 
 
@@ -15,6 +15,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
 
+# Set up the authentication type
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
@@ -26,6 +27,7 @@ elif AUTH_TYPE == "session_auth":
     auth = SessionAuth()
 
 
+# Error handlers
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -47,28 +49,36 @@ def forbidden(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
+# Before request filter
 @app.before_request
 def before_request():
     """Filter requests before processing"""
     if auth is None:
         return
 
+    # List of paths excluded from authentication
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
 
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    # Check if both authorization header and session cookie are missing
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         abort(401)
 
+    # Check if curre,t user exists; if not, abort with 403 Forbidden
     if auth.current_user(request) is None:
         abort(403)
 
+    # Attach the current user to the request
     request.current_user = auth.current_user(request)
 
 
+# Run the app
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
