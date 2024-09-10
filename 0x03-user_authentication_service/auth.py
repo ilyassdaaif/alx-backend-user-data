ac6ec7fd30dcc623a7199d8d0ed849cf476755b2
ilyassdf <ilyassdf@gmail.com>
2024-09-10 20:01:38 +0000

    a

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Auth module"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hash a password using bcrypt.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The salted hash of the input password.
+    """
+    # Convert the password string to bytes
+    password_bytes = password.encode('utf-8')
+
+    # Generate a salt and hash the password
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+
+    return hashed_password
