e2a28719215ff342d3dd92fea814ebd7752e582c
ilyassdf <ilyassdf@gmail.com>
2024-09-13 16:43:55 +0000

    p

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,29 +9,25 @@ app = Flask(__name__)
 AUTH = Auth()
 
 
-@app.route("/", methods=['GET'])
+@app.route('/', methods=['GET'])
 def welcome():
-    """Return a JSON payload for the root route"""
+    """Return a payload for the root route"""
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route("/users", methods=['POST'])
+@app.route('/users', methods=['POST'])
 def users():
     """Register a new user"""
-    # Get email and password from form data
     email = request.form.get('email')
     password = request.form.get('password')
-
     try:
-        # Register the user with provided email and password
         user = AUTH.register_user(email, password)
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
-        # If user is already registered, return an error message
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route("/sessions", methods=["POST"])
+@app.route('/sessions', methods=['POST'])
 def login():
     """Handle user login and session creation"""
     email = request.form.get('email')
