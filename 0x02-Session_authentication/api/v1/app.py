8f7ea7b80322092ff39700efccaacc480fb5c712
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-12-15 00:26:08 +0100

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,20 +2,20 @@
 """
 Route module for the API
 """
-from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import CORS
+from flask_cors import (CORS, cross_origin)
 import os
+from os import getenv
 
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
 
-# Set up the authentication type
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
@@ -25,38 +25,43 @@ elif AUTH_TYPE == "basic_auth":
 elif AUTH_TYPE == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
+elif AUTH_TYPE == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif AUTH_TYPE == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
-# Error handlers
-@app.errorhandler(404)
+@ app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
-def unauthorized(error) -> str:
+@ app.errorhandler(401)
+def unauthorized_error(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
-@app.errorhandler(403)
-def forbidden(error) -> str:
+@ app.errorhandler(403)
+def forbidden_error(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
-# Before request filter
-@app.before_request
-def before_request():
-    """Filter requests before processing"""
+@ app.before_request
+def before_request() -> str:
+    """ Before Request Handler
+    Requests Validation
+    """
     if auth is None:
         return
 
-    # List of paths excluded from authentication
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
                       '/api/v1/forbidden/',
@@ -65,20 +70,17 @@ def before_request():
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    # Check if both authorization header and session cookie are missing
-    if (auth.authorization_header(request) is None and
-            auth.session_cookie(request) is None):
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
 
-    # Check if curre,t user exists; if not, abort with 403 Forbidden
-    if auth.current_user(request) is None:
+    current_user = auth.current_user(request)
+    if current_user is None:
         abort(403)
 
-    # Attach the current user to the request
-    request.current_user = auth.current_user(request)
+    request.current_user = current_user
 
 
-# Run the app
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
